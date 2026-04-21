552f8f12e48321299eb9773cc81cb87d91b04dc7
boda0-mohie <bodemohie@gmail.com>
2026-04-21 17:19:34 +0200

    fix: DB connection error and db hosted successfully

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -5,11 +5,15 @@ export const typeOrmConfig = (
   configService: ConfigService,
 ): TypeOrmModuleOptions => ({
   type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_NAME'),
+  // host: configService.get<string>('DB_HOST'),
+  // port: configService.get<number>('DB_PORT'),
+  // username: configService.get<string>('DB_USERNAME'),
+  // password: configService.get<string>('DB_PASSWORD'),
+  // database: configService.get<string>('DB_NAME'),
+  url: configService.get<string>('DB_URL'),
+  ssl: {
+    rejectUnauthorized: false,
+  },
   autoLoadEntities: true,
   synchronize: true,
 });
